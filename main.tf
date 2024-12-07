9cc26d205d9f364fd660fed0c2e7b9fe80b5f558
TechSukeSuke <business.k.shimizu@gmail.com>
2024-12-07 16:35:07 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "instance_name" {
+  description = "The name of the instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
